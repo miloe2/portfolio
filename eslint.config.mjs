53309b42155b1eccb5d937b28281999405cf8495
miloe2 <miloe0117@gmail.com>
2025-02-11 15:19:40 +0900

    [Add] eslint config 수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,8 +3,8 @@ import pluginJs from "@eslint/js";
 import tseslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
 import pluginReact from "eslint-plugin-react";
-// import prettierPlugin from "eslint-plugin-prettier";
-// import prettierConfig from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
 
 export default [
   {
@@ -24,30 +24,29 @@ export default [
       },
     },
     plugins: {
-      // prettier: prettierPlugin,
+      prettier: prettierPlugin,
       react: pluginReact,
       "@typescript-eslint": tseslint,
       js: pluginJs,
     },
     rules: {
-      ...pluginJs.configs.recommended.rules,
-      ...tseslint.configs.recommended.rules,
-      ...pluginReact.configs.recommended.rules,
-      // ...prettierConfig.rules,
-
-      // // Prettier 규칙
-      // "prettier/prettier": [
-      //   "error",
-      //   {
-      //     endOfLine: "auto",
-      //     tabWidth: 2,  // Prettier와 일치하는 들여쓰기 설정
-      //     useTabs: false,
-      //   },
-      // ],
+      "prettier/prettier": [
+        "error",
+        {
+          endOfLine: "auto",
+          tabWidth: 2,
+          useTabs: false,
+          printWidth: 100, // 줄 길이 제한 추가
+        },
+      ],
       "react/react-in-jsx-scope": "off", // React 17+에서는 필요하지 않음
-      semi: ["error", "always"],
-      indent: ["error", 2], // Eslint에서 2 스페이스 들여쓰기
+      "react/jsx-indent": ["error", 2], // JSX 요소 들여쓰기 2 스페이스 적용
+      "react/jsx-indent-props": ["error", 2], // JSX 속성도 2 스페이스 들여쓰기
+      "react/jsx-closing-bracket-location": ["error", "line-aligned"], // JSX 닫기 위치 정리
+      "react/jsx-props-no-multi-spaces": "error", // JSX 속성 간 불필요한 공백 제거
+      "max-len": ["error", { "code": 100, "ignoreStrings": true, "ignoreTemplateLiterals": true }],
     },
+    
     settings: {
       react: {
         version: "detect",
