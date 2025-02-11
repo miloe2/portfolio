e295974e53d2d275c8f5ef3f8a9f970eb5b773eb
miloe2 <miloe0117@gmail.com>
2025-02-11 15:20:48 +0900

    [Refactor] Introudce / Career IO 적용

--- src/components/home/Career.tsx
+++ src/components/home/Career.tsx
@@ -1,11 +1,11 @@
-import { useRef, useState } from 'react';
-import useIntersectionObserver from '../../hooks/useIntersectionObserver';
-import CareerDescription from './CareerDescription';
-import { career, project, skills, education } from '../../assets/data/careerData';
+import { useRef, useState } from "react";
+import useIntersectionObserver from "../../hooks/useIntersectionObserver";
+import CareerDescription from "./CareerDescription";
+import { career, project, skills, education } from "../../assets/data/careerData";
 
 const Career = () => {
   const [isVisible, setIsVisible] = useState(false);
-  const handleVisibilityChange  = (_target : Element, isIntersecting : boolean) => {
+  const handleVisibilityChange = (_target: Element, isIntersecting: boolean) => {
     setIsVisible(isIntersecting);
   };
 
@@ -13,21 +13,24 @@ const Career = () => {
     { content: career, delay: "" },
     { content: project, delay: "delay-100" },
     { content: skills, delay: "delay-300" },
-    { content: education, delay: "delay-500" }
+    { content: education, delay: "delay-500" },
   ];
 
   const divRef = useRef(null);
-  useIntersectionObserver([divRef], handleVisibilityChange  );
+  useIntersectionObserver([divRef], handleVisibilityChange);
 
   return (
     <div
-      ref={divRef} 
+      ref={divRef}
       className={`
-        max-w-7xl w-full mx-auto mt-40 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-cols-1 gap-4 
-      `} 
+        max-w-7xl w-full mx-auto mt-40 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-cols-1 lg:gap-4 gap-16
+      `}
     >
       {careerList.map((item, index) => (
-        <div key={index} className={`transition-all duration-1000 transform delay ${item.delay} ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-20'}`}>
+        <div
+          key={index}
+          className={`transition-all duration-1000 transform delay ${item.delay} ${isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-20"}`}
+        >
           <CareerDescription content={item.content} />
         </div>
       ))}
--- src/components/home/Introduce.tsx
+++ src/components/home/Introduce.tsx
@@ -1,91 +1,67 @@
-import { useState, useEffect, useRef } from 'react';
-import pht1 from '../../assets/Images/photos/Int-pht1.webp';
-import pht2 from '../../assets/Images/photos/Int-pht2.webp';
+import { useState, useRef } from "react";
+import pht1 from "../../assets/Images/photos/Int-pht1.webp";
+import pht2 from "../../assets/Images/photos/Int-pht2.webp";
+import useIntersectionObserver from "../../hooks/useIntersectionObserver";
 
 const Introduce = () => {
-
   const [isTextVisible, setTextVisible] = useState(false);
   const [isImageVisible, setImageVisible] = useState(false);
 
   const textRef = useRef(null);
   const imageRef = useRef(null);
 
-  useEffect(() => {
-    const textObserver = new IntersectionObserver(
-      (entries) => {
-        if (entries[0].isIntersecting) {
-          setTextVisible(true);
-          // 이미지 애니메이션을 위한 setTimeout
-          setTimeout(() => {
-            const imageObserver = new IntersectionObserver(
-              (entries) => {
-                if (entries[0].isIntersecting) {
-                  setImageVisible(true);
-                }
-              },
-              { threshold: 0.1 }
-            );
-
-            if (imageRef.current) {
-              imageObserver.observe(imageRef.current);
-            }
-          }, 200); // 1초 후 이미지 애니메이션 시작 (시간 조절 가능)
-        }
-      },
-      { threshold: 0.2 }
-    );
-
-    if (textRef.current) {
-      textObserver.observe(textRef.current);
+  const handleVisibilityChange = (target: Element, isIntersecting: boolean) => {
+    if (target === textRef.current) {
+      setTextVisible(isIntersecting);
+    } else if (target === imageRef.current) {
+      setImageVisible(isIntersecting);
     }
+  };
 
-    return () => {
-      if (textRef.current) {
-        textObserver.unobserve(textRef.current);
-      }
-      if (imageRef.current) {
-        textObserver.unobserve(imageRef.current);
-      }
-    };
-  }, []);
+  useIntersectionObserver([textRef, imageRef], handleVisibilityChange);
+
+  const texts = [
+    { text: "Similique", class: "", delay: "delay-300" },
+    { text: "Lorem um", class: "text-right", delay: "delay-500" },
+    { text: "dolor sitip,", class: "text-cente", delay: "delay-700" },
+    { text: "consectetr", class: "text-center pl-[10%]", delay: "delay-1000" },
+  ];
 
   return (
-    //  bg-green-500 lg:bg-red-500 2xl:bg-blue-500 
-    <div className='w-full flex flex-col max-w-sm lg:max-w-2xl 2xl:max-w-4xl mx-auto bg-green-00'>
+    //  bg-green-500 lg:bg-red-500 2xl:bg-blue-500
+    <div className="w-full flex flex-col max-w-sm lg:max-w-2xl 2xl:max-w-4xl mx-auto bg-green-00">
       <div
+        ref={textRef}
         className={`
-          ${!isTextVisible ? 'opacity-0 translate-y-10' : 'opacity-100 translate-y-0'}
-          w-full h-full mx-auto text-introduce-heading font-bold leading-snug text-[#242424] transition-all relative duration-0000  whitespace-nowrap `}
+          w-full h-full mx-auto  bg-red-00 `}
       >
-        <div
-          className='space-y-2'
-          ref={textRef}>
-          <p>Similique.</p>
-          <p className='text-right'>Lorem um</p>
-          <p className='text-center'>dolor sitip,</p>
-          <p className='text-center pl-[10%]'>consectetr</p>
-        </div>
+        {texts.map((item, index) => (
+          <p
+            key={index}
+            className={`
+              ${item.class} transition-all z-10 duration-1000 transform text-introduce-heading font-bold leading-snug text-[#242424] relative whitespace-nowrap 
+              ${item.delay} 
+              ${isTextVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"}
+            `}
+          >
+            {item.text}
+          </p>
+        ))}
       </div>
-      <div 
+      <div
         ref={imageRef}
         className={`
-        ${!isImageVisible ? 'opacity-0 translate-y-10' : 'opacity-100  translate-y-0'}
-        min-w-60 w-1/2 self-end sm:translate-x-10 -translate-y-[10%] bg-red-500 ring-4 opacity-20`} >
-        <img
-          src={pht1}
-          alt=""
-          className="w-full h-full object-cover transition-all duration-1000 object-bottom"
-        />
+        ${isImageVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-40"}
+        min-w-60 w-1/2 self-end sm:translate-x-10 -translate-y-[10%] transition-all duration-1000 -top-10 relative`}
+      >
+        <img src={pht1} alt={pht1} className="w-full h-full object-cover  opacity-10" />
       </div>
-      <div 
+      <div
         className={`
-          ${!isImageVisible ? 'opacity-0 translate-y-0' : 'opacity-100 translate-y-0'}
-        min-w-64 w-1/2  bg-blue-500`}>
-        <img
-          src={pht2}
-          alt=""
-          className="w-full h-full object-cover transition-all duration-1000 opacity-10"
-        />
+        ${isImageVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-20"}
+        min-w-64 w-1/2 delay-1000 duration-1000`}
+      >
+        <img src={pht2} alt={pht2} className="w-full h-full object-cover opacity-10" />
       </div>
     </div>
   );
