e5c2be159f1b97f50d35ac1017db459d8ebf549e
miloe2 <miloe0117@gmail.com>
2025-02-14 17:05:53 +0900

    [Refactor] Left1Image Component 리팩토링

--- src/components/detail/Left1Image.tsx
+++ src/components/detail/Left1Image.tsx
@@ -1,41 +1,26 @@
-import React from 'react';
+import React from "react";
+import TitleText from "../common/TitleText";
+import Text from "../common/Text";
 
 interface Left1ImageProps {
-    bgColor: string;
-    img1: string;
-    title : string;
-    desc : string;
-    txtColor :string;
-
+  bgColor: string;
+  img1: string;
+  title: string;
+  desc: string;
+  txtColor: string;
 }
 
-const Left1Image :React.FC<Left1ImageProps> = ({ bgColor, txtColor, img1, title, desc }) => {
+const Left1Image: React.FC<Left1ImageProps> = ({ bgColor, txtColor, img1, title, desc }) => {
   return (
-    <div className='lg:w-[1240px] w-full h-full flex lg:flex-row flex-col-reverse relative justify-center items-center mx-auto mb-60'>
-      <div
-        className={`lg:w-2/3 w-full h-auto   flex justify-center items-start flex-col lg:p-0 p-6`}
-        style={{ backgroundColor : bgColor}}>
-        <img
-          src={img1}
-          alt=""
-          className='w-full h-full '/>
+    <div className="max-w-5xl mx-auto flex flex-col-reverse lg:flex-row ">
+      <div className={`lg:mr-10 mr-0 p-6`} style={{ backgroundColor: bgColor }}>
+        <img src={img1} alt={img1} className="" />
       </div>
-      <div className={`w-1/2 h-full  flex justify-center items-center flex-col`}>
-        <div className='w-72 h-auto '>
-          <div
-            className='lg:text-4xl/12 text-3xl/10  font-bold whitespace-pre-line  py-2 '
-            style={{ color: txtColor }}> 
-            {' '}
-            {title}
-            {' '}
-          </div>
-          <div className='lg:text-base/8 text-sm/6 whitespace-pre-line mb-10 lg:mb-0 '> 
-            {desc}
-          </div>
-        </div>
+      <div className={`flex flex-col max-w-[360px] mx-auto lg:mb-0 mb-6`}>
+        <TitleText title={title} txtColor={txtColor} />
+        <Text desc={desc} />
       </div>
     </div>
-
   );
 };
 
