bfa7bab744ea59324206a8d7ff48f0a48aa7b9bb
miloe2 <miloe0117@gmail.com>
2025-02-17 10:40:24 +0900

    [Refactor] RightImage refactoring

--- src/components/detail/RightFullImage.tsx
+++ src/components/detail/RightFullImage.tsx
@@ -1,44 +1,37 @@
-import React from 'react';
+import React from "react";
+import TitleText from "../common/TitleText";
+import Text from "../common/Text";
 
 interface RightFullImageProps {
-    bgColor: string;
-    title : string;
-    desc : string;
-    img : string;
-    txtColor :string,
-    descColor : string,
+  bgColor: string;
+  title: string;
+  desc: string;
+  img: string;
+  txtColor: string;
+  descColor: string;
 }
-const RightFullImage :React.FC<RightFullImageProps> = ({bgColor, txtColor, descColor, title, desc, img}) => {
+const RightFullImage: React.FC<RightFullImageProps> = ({
+  bgColor,
+  txtColor,
+  descColor,
+  title,
+  desc,
+  img,
+}) => {
   return (
-    <div className={`lg:w-[1240px] w-full mx-auto flex flex-col lg:flex-row relative justify-end items-center mb-60 `}>
-            
-      <div className={`lg:w-1/2 w-full h-full  flex justify-center items-center flex-col  `}>
-        <div
-          className={`w-full py-32 flex justify-center items-center lg:items-start flex-col  p-16`}
-          style={{ backgroundColor : bgColor}}>
-          <div
-            className={`lg:text-4xl/12 text-3xl/10 font-bold whitespace-pre-line `}
-            style={{ color: txtColor }}> 
-            {' '}
-            {title}
-            {' '}
-          </div>
-          <div
-            className={`lg:text-base/8 text-sm/6 whitespace-pre-line mt-5 `}
-            style={{ color: descColor }}> 
-            {desc}
-          </div>
-        </div>
+    <div
+      className={`max-w-7xl w-full mx-auto flex flex-col lg:flex-row relative justify-end items-center mb-60 `}
+    >
+      <div
+        className={`lg:w-1/2 w-full h-full p-16 flex items-start flex-col `}
+        style={{ backgroundColor: bgColor }}
+      >
+        <TitleText title={title} txtColor={txtColor} />
+        <Text desc={desc} txtColor={descColor} />
       </div>
-      <div className={`lg:w-1/2 w-full h-3/4 flex justify-start items-start  `}>
-        <div className='w-full h-full'>
-          <img
-            src={img}
-            alt=""
-            className='w-full h-full object-left-top object-cover shadow-md'/>
-        </div>
+      <div className={`lg:w-1/2 w-full h-3/4 flex justify-start items-start`}>
+        <img src={img} alt={img} className="w-full h-full object-left-top object-cover shadow-md" />
       </div>
-            
     </div>
   );
 };
--- src/components/detail/RightImage.tsx
+++ src/components/detail/RightImage.tsx
@@ -1,44 +1,40 @@
-import React from 'react';
+import React from "react";
+import TitleText from "../common/TitleText";
+import Text from "../common/Text";
 
 interface RightImageLeftProps {
-    bgColor: string;
-    title : string;
-    desc : string;
-    img : string;
-    items : string;
-    txtColor :string;
+  bgColor: string;
+  title: string;
+  desc: string;
+  img: string;
+  items: string;
+  txtColor: string;
 }
 
-
-const RightImage :React.FC<RightImageLeftProps> = ({bgColor, txtColor, title, desc, img, items}) => {
+const RightImage: React.FC<RightImageLeftProps> = ({
+  bgColor,
+  txtColor,
+  title,
+  desc,
+  img,
+  items,
+}) => {
   return (
-    <div className=' lg:w-[1240px]  w-full mx-auto h-full flex lg:flex-row flex-col relative justify-end items-center mb-60'>
+    <div className="max-w-7xl w-full mx-auto h-full flex lg:flex-row flex-col relative justify-end items-center mb-60">
       <div className={`lg:w-1/2 w-full h-full flex justify-center items-center flex-col mx-auto`}>
-        <div className='w-80 h-auto lg:mb-0 mb-10 '>
-          <div
-            className='lg:text-4xl/12 text-3xl/10 font-bold whitespace-pre-line  py-2 '
-            style={{ color: txtColor }}> 
-            {' '}
-            {title}
-            {' '}
-          </div>
-          <div className='lg:text-base/8 text-sm/6 whitespace-pre-line mt-5'> 
-            {desc}
-          </div>
+        <div className="w-80 h-auto lg:mb-0 mb-10 ">
+          <TitleText title={title} txtColor={txtColor} />
+          <Text desc={desc} />
         </div>
       </div>
       <div
-        className={`lg:w-1/2 w-full h-auto lg:py-40 py-20 flex justify-center items-${items} flex-col`}
-        style={{ backgroundColor : bgColor}}>
-        <div className='lg:w-3/4 w-full h-auto'>
-          <img
-            src={img}
-            alt=""
-            className='w-full h-full object-cover'/>
+        className={`lg:w-1/2 w-full h-auto py-20 flex justify-center items-${items} flex-col`}
+        style={{ backgroundColor: bgColor }}
+      >
+        <div className="lg:w-11/12 w-full h-auto">
+          <img src={img} alt="" className="w-full h-full object-cover" />
         </div>
-
       </div>
-            
     </div>
   );
 };
