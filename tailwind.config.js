b61992e7ae9655fca73c3ba15952e5affe0f47d3
miloe2 <miloe0117@gmail.com>
2025-02-19 10:16:14 +0900

    [Add] tailwind spacing 함수 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,126 +1,62 @@
+function generateFractions(limit = 12) {
+  const fractions = {};
+  for (let i = 1; i <= limit; i++) {
+    fractions[`${i}/${limit}`] = `${(i / limit) * 100}%`;
+  }
+  return fractions;
+}
+
+function generateSpacing(start = 100, end = 200, step = 4) {
+  const spacing = {};
+  for (let i = start; i <= end; i += step) {
+    spacing[i] = `${i / 4}rem`; // 1rem == 4를 기반으로
+  }
+  return spacing;
+}
+
 /** @type {import('tailwindcss').Config} */
 
-import scrollbarHide from 'tailwind-scrollbar-hide';
+import scrollbarHide from "tailwind-scrollbar-hide";
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontSize: {
-        "hello-heading" : "clamp(3rem, 9vw, 8.5rem)",
-        "hello-title" : "clamp(1.875rem, 3vw, 3rem)",
-        "introduce-heading" : "clamp(3rem, 6.5vw, 8rem)",
+        "hello-heading": "clamp(3rem, 9vw, 8.5rem)",
+        "hello-title": "clamp(1.875rem, 3vw, 3rem)",
+        "introduce-heading": "clamp(3rem, 6.5vw, 8rem)",
+        "exp-title": "clamp(2.1rem, 9vw, 8.5rem)",
       },
       // animation
       animation: {
-        bounce: 'bounce 1.5s infinite',
-        'my-animation': 'my-animation 0.5s forwards',
+        bounce: "bounce 1.5s infinite ease-in-out",
+        "my-animation": "my-animation 0.5s forwards ease-out",
       },
 
-      // keyframe
+      // keyframes
       keyframes: {
-        //바운스 애니메이션
+        // 바운스 애니메이션
         bounce: {
-          '0%, 100%': { transform: 'translateY(0px)' },
-          '5%': { transform: 'translateY(1px)' },
-          '10%': { transform: 'translateY(2px)' },
-          '15%': { transform: 'translateY(1px)' },
-          '20%': { transform: 'translateY(0px)' },
-          '25%': { transform: 'translateY(-1px)' },
-          '30%': { transform: 'translateY(-2px)' },
-          '35%': { transform: 'translateY(-1px)' },
-          '40%': { transform: 'translateY(0px)' },
-          '45%': { transform: 'translateY(1px)' },
-          '50%': { transform: 'translateY(2px)' },
-          '55%': { transform: 'translateY(1px)' },
-          '60%': { transform: 'translateY(0px)' },
-          '65%': { transform: 'translateY(-1px)' },
-          '70%': { transform: 'translateY(-2px)' },
-          '75%': { transform: 'translateY(-1px)' },
-          '80%': { transform: 'translateY(0px)' },
-          '85%': { transform: 'translateY(1px)' },
-          '90%': { transform: 'translateY(2px)' },
-          '95%': { transform: 'translateY(1px)' },
+          "0%, 100%": { transform: "translateY(0)" },
+          "20%": { transform: "translateY(2px)" },
+          "40%": { transform: "translateY(-2px)" },
+          "60%": { transform: "translateY(2px)" },
+          "80%": { transform: "translateY(-2px)" },
         },
-        'my-animation': {
-          '0%': { 
-            clipPath: 'polygon(100% 0, 100% 0, 100% 100%, 100% 100%)',
-          },
-          // '50%': { 
-          //   clipPath: 'polygon(100% 0, 30% 0, 30% 100%, 100% 100%)',
-          // },
-          '60%': { 
-            clipPath: 'polygon(100% 0, 20% 0, 40% 100%, 100% 100%)',
-          },
-          '70%': { 
-            clipPath: 'polygon(100% 0, 15% 0, 30% 100%, 100% 100%)',
-          },
-          '80%': { 
-            clipPath: 'polygon(100% 0, 10% 0, 20% 100%, 100% 100%)',
-          },
-          '90%': { 
-            clipPath: 'polygon(100% 0, 5% 0, 10% 100%, 100% 100%)',
-          },
-          '100%': {
-            clipPath: 'polygon(100% 0, 0% 0, 0% 100%, 100% 100%)'
-          }
+
+        "my-animation": {
+          "0%": { clipPath: "polygon(100% 0, 100% 0, 100% 100%, 100% 100%)" },
+          "60%": { clipPath: "polygon(100% 0, 20% 0, 40% 100%, 100% 100%)" },
+          "80%": { clipPath: "polygon(100% 0, 10% 0, 20% 100%, 100% 100%)" },
+          "100%": { clipPath: "polygon(100% 0, 0% 0, 0% 100%, 100% 100%)" },
         },
       },
-      inset: {
-        '1/5': '20%', // 여기에 1/5를 위한 값 20% 추가
-        '1/12': '8.3333%',
-        '2/12': '16.6667%',
-        '3/12': '25%',
-        '4/12': '33.3333%',
-        '5/12': '41.6667%',
-        '6/12': '50%',
-        '7/12': '58.3333%',
-        '8/12': '66.6667%',
-        '9/12': '75%',
-        '10/12': '83.3333%',
-        '11/12': '91.6667%',
+      spacing: {
+        ...generateSpacing(),
+        ...generateFractions(),
       },
-      height: {
-        '1/12': '8.3333%',
-        '2/12': '16.6667%',
-        '3/12': '25%',
-        '4/12': '33.3333%',
-        '5/12': '41.6667%',
-        '6/12': '50%',
-        '7/12': '58.3333%',
-        '8/12': '66.6667%',
-        '9/12': '75%',
-        '10/12': '83.3333%',
-        '11/12': '91.6667%',
-      },
-      lineHeight: {
-        '12': '3.00rem', // 44px
-      },
-      rotate:{
-        '20': '20deg',
-      },
-      dropShadow: {
-        '5px': '5px 5px 5px rgba(0, 0, 0, 0.8)',
-      },
-      top:{
-        '1/5': '20%',
-      },
-      borderWidth: {
-        '1': '1px',
-      },
-      right:{
-        '1/5': '20%',
-      },
-      left:{
-        '1/5': '20%',
-      }
     },
   },
-  plugins: [
-    scrollbarHide,
-  ],
-}
-
+  plugins: [scrollbarHide],
+};
