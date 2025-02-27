d040b4772b3e2740570b570c143ca86e24a9a3cf
miloe2 <miloe0117@gmail.com>
2025-02-27 17:02:41 +0900

    [Feature] IO 컴포넌트 생성성

---
+++ src/components/common/IntersectionObserver.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useRef, ReactNode } from "react";
+
+type IntersectionObserverProps = {
+  children: ReactNode;
+  onIntersect: (isVisible: boolean) => void;
+  options?: IntersectionObserverInit;
+};
+
+const IntersectionObserver = ({ children, onIntersect, options }: IntersectionObserverProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    const observer = new window.IntersectionObserver(([entry]) => {
+      onIntersect(entry.isIntersecting);
+    }, options);
+
+    observer.observe(ref.current);
+
+    return () => {
+      if (ref.current) observer.unobserve(ref.current);
+    };
+  }, [onIntersect, options]);
+
+  return <div ref={ref}>{children}</div>;
+};
+
+export default IntersectionObserver;
