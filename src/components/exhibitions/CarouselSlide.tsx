e9695b3c3fdc4f955f15f7160a76ae5469e05908
miloe2 <miloe0117@gmail.com>
2025-03-07 10:45:47 +0900

    [Feature] swiper autoplay/stop 추가

--- src/components/exhibitions/CarouselSlide.tsx
+++ src/components/exhibitions/CarouselSlide.tsx
@@ -1,9 +1,9 @@
-// import { useEffect, useState } from "react";
+import { useState, useRef } from "react";
 import carouselData from "../../assets/data/CarouselData";
-// import { BsFillPauseFill, BsPlayFill } from "react-icons/bs";
+import { BsFillPauseFill, BsPlayFill } from "react-icons/bs";
 // import { MdKeyboardArrowRight, MdKeyboardArrowLeft } from "react-icons/md";
 // // import Swiper core and required modules
-import { Navigation, Pagination } from "swiper/modules";
+import { Autoplay, Pagination } from "swiper/modules";
 // // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
@@ -13,23 +13,38 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import TitleText from "../common/TitleText";
 import Text from "../common/Text";
-
+import type { Swiper as SwiperType } from "swiper"; // ✅ Swiper 타입 가져오기
 const bulletStyle = `
   .custom-pagination .swiper-pagination-bullet {
     width: 0.4rem;
     height: 0.4rem;
     background-color: white;
-    opacity: 1;
+    opacity: 0.7;
     border-radius: 999px;
     transition: all 0.5s;
   }
   .custom-pagination .swiper-pagination-bullet-active {
     background-color: #D81519;
     width: 1.5rem;
+    opacity: 1;
   }
 `;
 
 const CarouselSlide = () => {
+  const swiperRef = useRef<SwiperType | null>(null);
+  const [isPlaying, setIsPlaying] = useState(true); 
+
+  // 재생/일시정지
+  const handleToggleAutoplay = () => {
+    if (!swiperRef.current) return;
+    if (isPlaying) {
+      swiperRef.current.autoplay.stop(); 
+    } else {
+      swiperRef.current.autoplay.start(); 
+    }
+    setIsPlaying(!isPlaying);
+  };
+
   return (
     <section className="mx-auto mb-60 w-full px-10 xl:px-0">
       <style>{bulletStyle}</style>
@@ -37,7 +52,8 @@ const CarouselSlide = () => {
         <TitleText title={"주요프로젝트"} txtColor="#D81519" />
       </div>
       <Swiper
-        modules={[Navigation, Pagination]}
+        onSwiper={(swiper) => (swiperRef.current = swiper)}
+        modules={[Autoplay, Pagination]}
         spaceBetween={20}
         slidesPerView={1.5}
         loop={true}
@@ -45,6 +61,10 @@ const CarouselSlide = () => {
         pagination={{
           el: ".custom-pagination",
           clickable: true,
+        }}
+        autoplay={{
+          delay: 10000,
+          disableOnInteraction: false,
         }}
         breakpoints={{
           640: {
@@ -58,11 +78,11 @@ const CarouselSlide = () => {
       >
         {carouselData.map(({ imgUrl, title, desc, location }, i) => (
           <SwiperSlide key={i}>
-            <div className="relative w-full h-80">
+            <div className="relative w-full 2xl:h-96 h-80 cursor-pointer">
               <img src={imgUrl} alt="" className="w-full h-full object-cover rounded-lg" />
               <div className="absolute inset-0 bg-black opacity-60 rounded-lg" />
               <p
-                className="absolute text-white bg-red-00 max-w-xs w-full top-1/2 -translate-y-1/2
+                className="absolute text-white opacity-100 max-w-xs w-full top-1/2 -translate-y-1/2
                 left-1/2 -translate-x-1/2 px-4
                 md:left-16 md:translate-x-0 md:px-0
               "
@@ -74,7 +94,15 @@ const CarouselSlide = () => {
             </div>
           </SwiperSlide>
         ))}
-        <div className="custom-pagination bg-blue-00 bottom-20 absolute w-full flex justify-center"></div>
+        <div className="flex justify-center mt-4">
+          <div className="custom-pagination !w-auto" />
+          <button
+            className="mt-1 ml-2 text-white ring-1 ring-white opacity-70 rounded-full w-5 h-5 justify-center flex items-center"
+            onClick={handleToggleAutoplay}
+          >
+            {!isPlaying ? <BsPlayFill /> : <BsFillPauseFill />}
+          </button>
+        </div>
       </Swiper>
     </section>
   );
